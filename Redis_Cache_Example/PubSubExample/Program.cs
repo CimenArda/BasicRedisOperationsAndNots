c403fcc41f027d7f10008b62b335525b5fe17b07
ArdaCimen <cimenarda.yaz@gmail.com>
2025-01-03 13:04:10 +0300

    Notların Yüklenmesi

---
+++ Redis_Cache_Example/PubSubExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer  redis = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = redis.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Gönderilen Mesaj:");
+    string message = Console.ReadLine();
+
+   await subscriber.PublishAsync("mychannel",message);
+}
